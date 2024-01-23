2cea0db320350484eb1b96b506d2973b92f66a2e
GXC2356 <108477326+GXC2356@users.noreply.github.com>
2024-01-23 11:34:50 +0200

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y && sudo -H apt-get install bc python2 ccache binutils-aarch64-linux-gnu cpio && sudo apt install tmate && git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r510928.git ~/tc/clang-latest && git fetch https://github.com/crdroidandroid/android_kernel_xiaomi_sm8150.git
+sudo apt update && sudo apt upgrade -y && sudo -H apt-get install bc python2 ccache binutils-aarch64-linux-gnu cpio tmate gh -y && git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r510928.git ~/tc/clang-latest && git fetch https://github.com/crdroidandroid/android_kernel_xiaomi_sm8150.git
